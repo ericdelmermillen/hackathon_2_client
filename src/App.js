305b86779ee1a1e6fc6511cbec40ccf00b15e286
Eric <ericdelmermillen@gmail.com>
2023-08-11 00:32:19 -0400

    tesing ability to push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 function App() {
+  // setTimeout(console.log(transcript), 500)
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  SpeechRecognition.startListening({ continuous: true })
+
+  // console.log(listening)
+
+// start listening
+// SpeechRecognition.startListening()
+
+
+// stop listening
+// SpeechRecognition.stopListening()
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {`This is the transcript contents: ${transcript}`}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>{`Abe An An Appeal`}</h1>
+        <span className="word">{`${transcript.split(" ").includes("a") ? "a" : ""}`}</span>
+        <span className="word">{`${transcript.split(" ").includes("banana") ? "banana" : ""}`}</span>
+        <span className="word">{`${transcript.split(" ").includes("peel") ? "peel" : ""}`}</span>
       </header>
     </div>
-  );
-}
+  )};
 
 export default App;
